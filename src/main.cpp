2a2f5f06cfd17d854826d156b8dc2a168b1ae95c
Marek Bogusovsky <bobogu@protonmail.com>
2019-03-28 17:05:25 +0100

    Running two ssd1306 displays - one SPI, one I2C

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,8 @@
 #include <Wire.h>
 #endif
 
-U8G2_SSD1306_128X64_NONAME_F_SW_I2C u8g2(U8G2_R0, SCL, SDA, U8X8_PIN_NONE); // All Boards without Reset of the Display
+U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2_i2c(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
+U8G2_SSD1306_128X64_NONAME_F_4W_HW_SPI u8g2(U8G2_R0, /* cs=*/ 4, /* dc=*/ 3, /* reset=*/ 2);
 
 void togglePin(const int pin, const int delayMs) {
     digitalWrite(pin, HIGH);
@@ -20,6 +21,7 @@ void togglePin(const int pin, const int delayMs) {
 int main() {
     init();
     u8g2.begin();
+    u8g2_i2c.begin();
 
     static constexpr int pin = 5;
     pinMode(pin, OUTPUT);
@@ -27,11 +29,19 @@ int main() {
     while (true) {
         u8g2.clearBuffer();
         u8g2.setFont(u8g2_font_profont10_tf);
-        u8g2.drawStr(0,10, "Hi mate!");
-        u8g2.drawStr(0,20, "Finally got this working.");
-        u8g2.drawStr(0,30, "You wouldn't believe");
-        u8g2.drawStr(0,40, "where the problem was...");
+        u8g2.drawStr(0,10, "Hey, I am running on SPI.");
+        u8g2.drawStr(0,20, "I am fast as a lightning!");
+        u8g2.drawStr(0,30, "But I occupy many pins :(");
+        u8g2.drawStr(0,40, "What do you say, bro?");
         u8g2.sendBuffer();
+
+        u8g2_i2c.clearBuffer();
+        u8g2_i2c.setFont(u8g2_font_profont10_tf);
+        u8g2_i2c.drawStr(0,10, "Hey, I am running on I2C.");
+        u8g2_i2c.drawStr(0,20, "Yes, I am a bit slower...");
+        u8g2_i2c.drawStr(0,30, "But I only need 2 pins :)");
+        u8g2_i2c.drawStr(0,40, "Cool, right?");
+        u8g2_i2c.sendBuffer();
 
         togglePin(pin, 200);
     }
